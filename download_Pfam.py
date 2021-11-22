a9e8c19a2c199f1791e861b2091b2e9ceb8699b4
exsto <46631284+exsto1@users.noreply.github.com>
2021-11-23 00:16:40 +0100

    .

--- download_Pfam.py
+++ download_Pfam.py
@@ -1,14 +1,20 @@
 import random
 from urllib import request
 from tqdm import tqdm
+import os
 
-correct = 0
+correct = len(os.listdir("seq"))
 while correct < 100:
-    id = random.randint(10000, 99999)
+    id = f'{random.randint(1, 99999):05}'
     url = f"http://pfam.xfam.org/family/PF{id}/alignment/seed/format?format=fasta&alnType=seed&order=t&case=u&gaps=none&download=0"
     try:
-        request.urlretrieve(url, f"seq/PF{id}.fasta")
-        correct += 1
+        data = request.urlopen(url).read().decode("utf-8")
+        if data:
+            if data.count(">") > 10:
+                newfile = open(f"seq/PF{id}.fasta", "w")
+                newfile.write(data)
+                newfile.close()
+                correct += 1
     except:
         continue
     print(correct)
--- seq_join.py
+++ seq_join.py
@@ -1,6 +1,13 @@
-# import os
-#
-# files = os.listdir("seq")
-# for i in files:
-#
-#
+import os
+
+files = os.listdir("seq")
+for i in files:
+    file_h = open(f"seq/{i}")
+    file = file_h.read().split(">")
+    file_h.close()
+    file = "".join([f">{i.split('.')[0]}_" + i1 for i1 in file[1:31]])
+    newfile = open("temp_files/full_seq.fasta", "a")
+    newfile.write(file)
+    newfile.close()
+
+
