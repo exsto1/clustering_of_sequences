751f07d7875baffc1eff6fb3a194c3a09c0ceec8
exsto1 <exsto1@gmail.com>
2021-11-23 02:10:13 +0100

    .

---
+++ clustering.py
@@ -0,0 +1,6 @@
+import os
+
+# os.system(f"cd-hit -i temp_files/all_seq.fasta -o temp_files/cd-hit_clustering_local -G 0 -aS 0.4")
+os.system(f"sumaclust -h temp_files/all_seq.fasta")
+
+#os.system("mmseqs easy-search temp_files/all_seq.fasta temp_files/all_seq.fasta temp_files/all_seq.fasta temp_files/mmseqs_clustering")
--- seq_join.py
+++ seq_join.py
@@ -6,7 +6,7 @@ for i in files:
     file = file_h.read().split(">")
     file_h.close()
     file = "".join([f">{i.split('.')[0]}_" + i1 for i1 in file[1:31]])
-    newfile = open("temp_files/full_seq.fasta", "a")
+    newfile = open("temp_files/all_seq.fasta", "a")
     newfile.write(file)
     newfile.close()
 
